c5056e409ca83c7db340358f73c1b94b1b6a0b07
Khash <khash.021@gmail.com>
2018-09-08 16:20:57 -0600

    Added Forecast class

---
+++ app/src/main/java/app/khash/weathertry/Forecast.java
@@ -0,0 +1,45 @@
+package app.khash.weathertry;
+
+/**
+ * Object representing each forecast to be used for showing the list of forecasts
+ */
+
+//TODO: add some sunrise, sunset, maybe even pressure, and wind stuff
+
+public class Forecast {
+
+    private String day, summary, min, max, pop, precipType;
+
+    public Forecast (String summary, String min, String max, String pop, String precipType, String day) {
+        this.summary = summary;
+        this.min = min;
+        this.max = max;
+        this.pop = pop;
+        this.precipType = precipType;
+        this.day = day;
+    }//constructor
+
+    public String getDay() {
+        return day;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public String getMin() {
+        return min;
+    }
+
+    public String getMax() {
+        return max;
+    }
+
+    public String getPop() {
+        return pop;
+    }
+
+    public String getPrecipType() {
+        return precipType;
+    }
+}//class
