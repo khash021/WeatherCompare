5156e4f32976a6938772ff72d274aa6b1356d7a5
Khash <khash.021@gmail.com>
2019-08-21 11:34:33 -0700

    ADDED CONSTANT CLASS
    for storing app constants such as pref keys

---
+++ app/src/main/java/tech/khash/weathercompare/model/Constant.java
@@ -0,0 +1,19 @@
+package tech.khash.weathercompare.model;
+
+/**
+ * Created by Khashayar "Khash" Mortazavi
+ *
+ * This final class holds constants used throughout the app such as preference keys
+ *
+ */
+
+public final class Constant {
+
+    //Preference keys
+    public static final String PREF_KEY_ARRAY = "pref_key_array";
+
+    //location permission
+    public final static int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+
+}//Constant
